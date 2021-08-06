abf626437bc1e1f537f191deb32cead9b45388b1
Brad Vogel <brad@mixmax.com>
2021-08-06 15:35:45 -0400

    test: refactor tests to make a bit more readable

--- spec/firehoseLoggerSpec.js
+++ spec/firehoseLoggerSpec.js
@@ -3,15 +3,18 @@ const MockFireHoser = require('./support/mock-firehoser');
 const WFireHose = require('../src/index.js');
 
 describe('firehose logger transport', function () {
-  beforeAll(function () {
-    this.m = new MockFireHoser('test-stream', {});
-    this.message = 'test message';
-    spyOn(this.m, 'send').and.callThrough();
+  let mockFirehoser;
+  const message = 'test message';
+
+  beforeEach(function () {
+    jasmine.clock().mockDate(new Date('Fri Aug 06 2021 15:17:28 GMT-0400 (Eastern Daylight Time)'));
+
+    mockFirehoser = new MockFireHoser('test-stream', {});
+    spyOn(mockFirehoser, 'send').and.callThrough();
   });
 
   it('logs a message', function (done) {
-    const { m, message } = this;
-    m.send(message)
+    mockFirehoser.send(message)
       .then(response => {
         expect(response).toBe(message);
         done();
@@ -20,15 +23,14 @@ describe('firehose logger transport', function () {
   });
 
   it('affixes to winston', function () {
-    const { m, message } = this;
-    m.send.calls.reset();
     const logger = winston.createLogger({
       transports: [
-        new WFireHose({ firehoser: m }),
+        new WFireHose({ firehoser: mockFirehoser }),
       ],
     });
 
     logger.info(message);
-    expect(m.send).toHaveBeenCalled();
+
+    expect(mockFirehoser.send).toHaveBeenCalled();
   });
 });
